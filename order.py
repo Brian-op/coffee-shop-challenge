bea0d9439b8dbe5f319d04b3cc378c94924d6caa
Brian <brian.kiprono8@student.moringaschool.com>
2025-05-20 22:46:10 +0300

    relate orders to the rest

--- order.py
+++ order.py
@@ -1,8 +1,12 @@
 class Order :
+    all = []
+
     def __init__(self, customer, coffee, price):
-        self.customer_name = customer
-        self.coffee_name = coffee
-        self.coffee_price = price 
+        self._customer = customer
+        self._coffee = coffee
+        self._price = price
+
+        Order.all.append(self) 
 
         if not isinstance(price, float):
             raise ValueError ("Your price needs to be a float")
@@ -12,6 +16,14 @@ class Order :
    
     @property
     def price(self):
-        return self.coffee_price
+        return self._price
     
+    @property
+    def customer(self):
+        return self._customer
+        
+    @property
+    def coffee(self):
+        return self._coffee
     
+
